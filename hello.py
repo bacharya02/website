56db8a419d2d2f3cfaeeb8813b77a882640f6bd4
Bikash <bacharya02@gmail.com>
2015-07-27 23:50:53 -0400

    Heroku

---
+++ hello.py
@@ -0,0 +1,19 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return 'Hello World!'
+
+@app.route('/bikash')
+def hello_bikash():
+	a='Bikash can math ' 
+	f='<p>'	
+	b='2*4='
+	c=2*4
+	e=str(c)	
+	d=a + f + b + e	
+	return d
+
+if __name__=='__main__':
+	app.run()
